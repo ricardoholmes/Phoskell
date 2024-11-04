f9589169ecf4454cdaf97b6364f20f4f1dec1b37
ricardoholmes <ricardoholmes2004@gmail.com>
2024-11-04 14:57:11 +0000

    Add potential definition for image type

--- src/Graphics/Image.hs
+++ src/Graphics/Image.hs
@@ -1,6 +1,17 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Graphics.Image (
-    someFunc
+    Image(..),
+    ImageProcess(..),
 ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Massiv.Array as M
+
+data Image a where
+    BaseImage :: M.Array M.P M.Ix3 a -> Image a
+    (:>) :: Image x -> ImageProcess x a -> Image a
+
+data ImageProcess a b = PointProcess (a -> b)
+                      | FragmentProcess ((Int, Int, a) -> b)
+                      | GeneralProcess (Image a -> Image b)
