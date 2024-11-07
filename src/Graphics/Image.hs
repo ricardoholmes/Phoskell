c242d58122cb2155d0179d5ec279b940a90b55c2
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-11-07 13:07:42 +0000

    Annotate main types and all functions so far

--- src/Graphics/Image.hs
+++ src/Graphics/Image.hs
@@ -10,22 +10,32 @@ module Graphics.Image (
 
 import qualified Data.Massiv.Array as M
 
-type ImageArray a = M.Array M.D M.Ix3 a
+-- | Base array type for storing images
+type ImageArray a = M.Array M.D M.Ix2 a
 
-data ImageProcess a b = PointProcess (a -> b) -- point process
-                      | IPointProcess (M.Ix3 -> a -> b) -- index-aware point process
-                      | MiscProcess (ImageArray a -> ImageArray b) -- anything else
+-- | Wrapper type for any and all types of image processes
+data ImageProcess a b = -- | Point process
+                        PointProcess (a -> b) |
+                         -- | Index-aware point process
+                        IPointProcess (M.Ix2 -> a -> b) |
+                        -- | Anything else
+                        MiscProcess (ImageArray a -> ImageArray b)
 
+-- | Core image type for usage across most operations in this library
 data Image a where
+    -- | Base image constructor
     BaseImage :: ImageArray a -> Image a
+    -- | Pipeline constructor for linking together processes applied to an image
     (:>) :: Image x -> ImageProcess x a -> Image a
 
 infixl 5 :>
 
+-- | Generates the whole image, converting it into an array
 generateImage :: Image a -> ImageArray a
 generateImage (BaseImage img) = img
 generateImage (img :> f) = applyProcess f (generateImage img)
 
+-- | Applies a single process to an image array
 applyProcess :: ImageProcess a b -> ImageArray a -> ImageArray b
 applyProcess (PointProcess f) = M.map f
 applyProcess (IPointProcess f) = M.imap f
