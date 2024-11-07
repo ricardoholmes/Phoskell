82136fd35f050f266af6c6da2f65e377253ca8a7
ricardoholmes <ricardoholmes2004@gmail.com>
2024-11-07 18:15:46 +0000

    Add a function for reading an image with JuicyPixels
    The function also converts the image to this library's custom Image type.
    
    Running the executable with an image's file path as a command-line argument will
    now output that image's data (or an error if it fails).

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,13 @@
 module Main where
 
-import Graphics.Image ()
+import Graphics.Image (generateImage)
+import Graphics.IO (readImage)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
+main = do args <- getArgs
+          let fname = head args
+          img <- readImage fname
+          case img of
+            Left s -> putStrLn s
+            Right img' -> print (generateImage img')
