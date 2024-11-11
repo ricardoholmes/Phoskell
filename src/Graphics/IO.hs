2d672dcb9321939ab72c615d9bf6e13d7fbe034f
ricardoholmes <ricardoholmes2004@gmail.com>
2024-11-11 12:54:33 +0000

    Rewrite functions and types to use massiv-io
    Also, separate Image and Pipeline types.

--- src/Graphics/IO.hs
+++ src/Graphics/IO.hs
@@ -1,6 +1,15 @@
 module Graphics.IO (
-    readImage,
+    readImage
 ) where
 
-import Graphics.IO.JuicyPixels
-import Graphics.IO.Netpbm
+import qualified Data.Massiv.Array.IO as MIO
+import Data.Massiv.Array
+import Graphics.Image
+import Graphics.Pixel
+
+readImage :: (ColorModel cs e) => FilePath -> IO (Image cs e)
+readImage path = do img <- MIO.readImage path
+                    return $ massivToImage img
+
+massivToImage :: MIO.Image S cs e -> Image cs e
+massivToImage = Image
