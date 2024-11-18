f3297fbc3caf54211c37bbe8e8df27fc5e2d6c24
ricardoholmes <ricardoholmes2004@gmail.com>
2024-11-18 18:03:22 +0000

    Add functions for reading and writing RGBA and Grayscale images

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,15 @@
 module Main where
 
 import System.Environment (getArgs)
-import Graphics.Image.IO (readImageRGB, writeImageRGB)
+import Graphics.Image.IO
 import Graphics.Image.ImageProcess (PointProcess(PointProcess))
 import Graphics.Image (Image(..))
+import Graphics.Image.Color (rgbToGray)
 
 main :: IO ()
 main = do args <- getArgs
           let fname = head args
           img <- readImageRGB fname
           let img' = img :> PointProcess (1-)
-          writeImageRGB "out.png" img'
+          writeImageRGB "out-inverted.png" img'
+          writeImageGray "out-gray.png" (img :> PointProcess rgbToGray)
