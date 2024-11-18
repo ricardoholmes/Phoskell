379ca7de0bb045db43e094b249b3154bdb9f9c30
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-11-18 18:03:22 +0000

    Add functions for reading and writing RGBA and Grayscale images

--- src/Graphics/Image/IO.hs
+++ src/Graphics/Image/IO.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE DataKinds #-}
 module Graphics.Image.IO (
+    readImageGray,
     readImageRGB,
+    readImageRGBA,
+    writeImageGray,
     writeImageRGB,
+    writeImageRGBA,
 ) where
 
 import qualified Data.Massiv.Array as M
@@ -13,6 +17,16 @@ import Graphics.Image.Internal
 import Graphics.Image.ImageProcess (PointProcess(PointProcess))
 import Data.Ord (clamp)
 
+-- Read Images --
+
+readImageGray :: FilePath -> IO (Image Gray)
+readImageGray fp = do img <- readImageRGB' fp
+                      let img' = M.map (\(MIO.PixelY' y) -> Pixel1 y) img
+                      return $ BaseImage img'
+        where
+            readImageRGB' :: FilePath -> IO (MIO.Image M.S (MIO.Y' MIO.SRGB) Double)
+            readImageRGB' = MIO.readImageAuto
+
 readImageRGB :: FilePath -> IO (Image RGB)
 readImageRGB fp = do img <- readImageRGB' fp
                      let img' = M.map (\(MIO.PixelSRGB r g b) -> Pixel3 r g b) img
@@ -21,8 +35,33 @@ readImageRGB fp = do img <- readImageRGB' fp
             readImageRGB' :: FilePath -> IO (MIO.Image M.S (MIO.SRGB MIO.NonLinear) Double)
             readImageRGB' = MIO.readImageAuto
 
+readImageRGBA :: FilePath -> IO (Image RGBA)
+readImageRGBA fp = do img <- readImageRGBA' fp
+                      let img' = M.map (\(MIO.PixelSRGBA r g b a) -> Pixel4 r g b a) img
+                      return $ BaseImage img'
+        where
+            readImageRGBA' :: FilePath -> IO (MIO.Image M.S (MIO.Alpha (MIO.SRGB MIO.NonLinear)) Double)
+            readImageRGBA' = MIO.readImageAuto
+
+-- Write Images --
+
+writeImageGray :: FilePath -> Image Gray -> IO ()
+writeImageGray fp img = MIO.writeImageAuto fp
+                     $ M.map grayToY'
+                     $ toArray
+                     $ img :> PointProcess (fmap (clamp (0,1)))
+        where
+            grayToY' :: Gray -> MIO.Pixel (MIO.Y' MIO.SRGB) Double
+            grayToY' (Pixel1 y) = MIO.PixelY' y
+
 writeImageRGB :: FilePath -> Image RGB -> IO ()
 writeImageRGB fp img = MIO.writeImageAuto fp
                      $ M.map (\(Pixel3 r g b) -> MIO.PixelSRGB r g b)
+                     $ toArray
+                     $ img :> PointProcess (fmap (clamp (0,1)))
+
+writeImageRGBA :: FilePath -> Image RGBA -> IO ()
+writeImageRGBA fp img = MIO.writeImageAuto fp
+                     $ M.map (\(Pixel4 r g b a) -> MIO.PixelSRGBA r g b a)
                      $ toArray
                      $ img :> PointProcess (fmap (clamp (0,1)))
