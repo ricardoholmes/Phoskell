6311b92437cfe66cbf5c218a693623e59c3ae843
ricardoholmes <ricardoholmes2004@gmail.com>
2024-11-18 16:09:44 +0000

    Make IO functions automatically convert color space

--- src/Graphics/Image/IO.hs
+++ src/Graphics/Image/IO.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DataKinds #-}
 module Graphics.Image.IO (
     readImageRGB,
     writeImageRGB,
@@ -5,20 +6,20 @@ module Graphics.Image.IO (
 
 import qualified Data.Massiv.Array as M
 import qualified Data.Massiv.Array.IO as MIO
-import qualified Graphics.Color.Model as C
-import Graphics.Image
+
 import Graphics.Image.Color
 import Graphics.Image.Pixel
 import Graphics.Image.Internal
-    
+
 readImageRGB :: FilePath -> IO (Image RGB)
-readImageRGB fp = do img <- MIO.readImage fp
-                     let img' = M.map (\p ->
-                            let (C.ColorRGB r g b) = MIO.pixelColor p
-                            in Pixel3 r g b) img
+readImageRGB fp = do img <- readImageRGB' fp
+                     let img' = M.map (\(MIO.PixelSRGB r g b) -> Pixel3 r g b) img
                      return $ BaseImage img'
+        where
+            readImageRGB' :: FilePath -> IO (MIO.Image M.S (MIO.SRGB MIO.NonLinear) Double)
+            readImageRGB' = MIO.readImageAuto
 
 writeImageRGB :: FilePath -> Image RGB -> IO ()
-writeImageRGB fp = MIO.writeImage fp .
-        M.map (\(Pixel3 r g b) -> MIO.Pixel $ C.ColorRGB r g b) .
+writeImageRGB fp = MIO.writeImageAuto fp .
+        M.map (\(Pixel3 r g b) -> MIO.PixelSRGB r g b) .
         toArray
