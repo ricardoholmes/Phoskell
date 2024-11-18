e92a13b438a734c119d8412af2219cba259e73c5
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-11-18 12:49:19 +0000

    Define colours in terms of Pixel types
    Also, add functions for converting between types

--- src/Graphics/Image/Color.hs
+++ src/Graphics/Image/Color.hs
@@ -1,51 +1,146 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
 module Graphics.Image.Color (
-    dropAlpha,
-    addAlpha,
-
+    Gray,
+    RGB,
     RGBA,
-    toRGB,
-    toHSI,
-    toHSL,
-    toHSV,
-    toYCbCr,
-    toCMYK,
+    HSV,
+    HSL,
+
+    grayToRGB,
+    grayToRGBA,
+    grayToHSV,
+    grayToHSL,
+
+    rgbToGray,
+    rgbToRGBA,
+    rgbToHSV,
+    rgbToHSL,
+
+    rgbaToGray,
+    rgbaToRGB,
+    rgbaToHSV,
+    rgbaToHSL,
+
+    hsvToGray,
+    hsvToRGB,
+    hsvToRGBA,
+    hsvToHSL,
+
+    hslToGray,
+    hslToRGB,
+    hslToRGBA,
+    hslToHSV,
 ) where
 
-import Graphics.Color.Space (ColorSpace)
-import Graphics.Pixel (Pixel, liftPixel)
-import Graphics.Color.Model hiding (dropAlpha, addAlpha)
-import qualified Graphics.Color.Model as GCM
-import Graphics.Color.Adaptation (convert)
+import Graphics.Image.Pixel
+import Data.Fixed (mod')
 
-type RGBA = Alpha RGB
+--- type aliases ---
 
-dropAlpha :: Pixel (Alpha cs') e' -> Pixel cs' e'
-dropAlpha = liftPixel GCM.dropAlpha
+type Gray = Pixel1 Double
+type RGB = Pixel3 Double
+type RGBA = Pixel4 Double
+type HSV = Pixel3 Double
+type HSL = Pixel3 Double
 
-addAlpha :: Pixel (Alpha cs') e' -> Pixel cs' e'
-addAlpha = liftPixel GCM.dropAlpha
+--- color space conversion ---
 
--- | Convert to RGB color space
-toRGB :: (ColorSpace cs i e, ColorSpace RGB i' e) => Pixel cs e -> Pixel RGB e
-toRGB = liftPixel convert
+-- from gray
+grayToRGB :: Gray -> RGB
+grayToRGB (Pixel1 x) = Pixel3 x x x
 
--- | Convert to HSI color space
-toHSI :: (ColorSpace cs i e, ColorSpace HSI i' e) => Pixel cs e -> Pixel HSI e
-toHSI = liftPixel convert
+grayToRGBA :: Gray -> RGBA
+grayToRGBA (Pixel1 x) = Pixel4 x x x 1
 
--- | Convert to HSL color space
-toHSL :: (ColorSpace cs i e, ColorSpace HSL i' e) => Pixel cs e -> Pixel HSL e
-toHSL = liftPixel convert
+grayToHSV :: Gray -> HSV
+grayToHSV = rgbToHSV . grayToRGB
 
--- | Convert to HSV color space
-toHSV :: (ColorSpace cs i e, ColorSpace HSV i' e) => Pixel cs e -> Pixel HSV e
-toHSV = liftPixel convert
+grayToHSL :: Gray -> HSL
+grayToHSL = rgbToHSL . grayToRGB
 
--- | Convert to YCbCr color space
-toYCbCr :: (ColorSpace cs i e, ColorSpace YCbCr i' e) => Pixel cs e -> Pixel YCbCr e
-toYCbCr = liftPixel convert
+-- from rgb
+rgbToGray :: RGB -> Gray
+rgbToGray = dot3 (Pixel3 0.299 0.587 0.114)
 
--- | Convert to CMYK color space
-toCMYK :: (ColorSpace cs i e, ColorSpace CMYK i' e) => Pixel cs e -> Pixel CMYK e
-toCMYK = liftPixel convert
+rgbToRGBA :: RGB -> RGBA
+rgbToRGBA (Pixel3 r g b) = Pixel4 r g b 1
+
+rgbToHSV :: RGB -> HSV
+rgbToHSV (Pixel3 r g b) = Pixel3 h s v
+    where
+        ma = max r (max g b)
+        mi = min r (min g b)
+        c = ma - mi
+        h' | c == 0    = 0
+            | ma == r   = ((g - b)/c) `mod'` 6
+            | ma == g   = ((b - r)/c) + 2
+            | otherwise = ((r - g)/c) + 4 -- ma == b
+        h = 60 * h'
+        v = ma
+        s = if v == 0 then 0 else c/v
+
+rgbToHSL :: RGB -> HSL
+rgbToHSL (Pixel3 r g b) = Pixel3 h s l
+    where
+        ma = max r (max g b)
+        mi = min r (min g b)
+        c = ma - mi
+        h' | c == 0    = 0
+            | ma == r   = ((g - b)/c) `mod'` 6
+            | ma == g   = ((b - r)/c) + 2
+            | otherwise = ((r - g)/c) + 4 -- ma == b
+        h = 60 * h'
+        l = (ma - mi) / 2
+        s = if l == 0 then 0 else c/(1 - abs (2*l - 1))
+
+-- from rgba
+rgbaToGray :: RGBA -> Gray
+rgbaToGray = rgbToGray . rgbaToRGB
+
+rgbaToRGB :: RGBA -> RGB
+rgbaToRGB (Pixel4 r g b a) = multScalar3 a (Pixel3 r g b)
+
+rgbaToHSV :: RGBA -> HSV
+rgbaToHSV = rgbToHSV . rgbaToRGB
+
+rgbaToHSL :: RGBA -> HSL
+rgbaToHSL = rgbToHSL . rgbaToRGB
+
+-- from hsv
+hsvToGray :: HSV -> Gray
+hsvToGray = rgbToGray . hsvToRGB
+
+hsvToRGB :: HSV -> RGB
+hsvToRGB (Pixel3 h s v) = Pixel3 (f 5) (f 3) (f 1)
+    where f n = let k = (n + h/60) `mod'` 6
+                in v - v * s * max 0 (min k (min (4-k) 1))
+
+hsvToRGBA :: HSV -> RGBA
+hsvToRGBA = rgbToRGBA . hsvToRGB
+
+hsvToHSL :: HSV -> HSL
+hsvToHSL (Pixel3 h sv v) = Pixel3 h sl l
+    where
+        l = v * (1 - (sv / 2))
+        sl = if l == 0 || l == 1 then 0 else (v - l) / min l (1-l)
+
+-- from hsv
+hslToGray :: HSL -> Gray
+hslToGray = rgbToGray . hslToRGB
+
+hslToRGB :: HSL -> RGB
+hslToRGB (Pixel3 h s l) = Pixel3 (f 0) (f 8) (f 4)
+    where
+        f n = let k = (n + h/30) `mod'` 12
+              in l - a * max (-1) (min (k-3) (min (9-k) 1))
+        a = s * min l (1 - l)
+
+hslToRGBA :: HSL -> RGBA
+hslToRGBA = rgbToRGBA . hslToRGB
+
+hslToHSV :: HSL -> HSV
+hslToHSV (Pixel3 h sl l) = Pixel3 h sv v
+    where
+        v = l + sl * min 1 (1 - l)
+        sv = if v == 0 then 0 else 2 * (1 - l/v)
