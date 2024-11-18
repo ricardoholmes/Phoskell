1501d2e3f83c91b8527601d86138183b2b8aabad
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-11-18 16:27:28 +0000

    Stop application halting indefinitely on negative pixels

--- src/Graphics/Image/IO.hs
+++ src/Graphics/Image/IO.hs
@@ -10,6 +10,8 @@ import qualified Data.Massiv.Array.IO as MIO
 import Graphics.Image.Color
 import Graphics.Image.Pixel
 import Graphics.Image.Internal
+import Graphics.Image.ImageProcess (PointProcess(PointProcess))
+import Data.Ord (clamp)
 
 readImageRGB :: FilePath -> IO (Image RGB)
 readImageRGB fp = do img <- readImageRGB' fp
@@ -20,6 +22,7 @@ readImageRGB fp = do img <- readImageRGB' fp
             readImageRGB' = MIO.readImageAuto
 
 writeImageRGB :: FilePath -> Image RGB -> IO ()
-writeImageRGB fp = MIO.writeImageAuto fp .
-        M.map (\(Pixel3 r g b) -> MIO.PixelSRGB r g b) .
-        toArray
+writeImageRGB fp img = MIO.writeImageAuto fp
+                     $ M.map (\(Pixel3 r g b) -> MIO.PixelSRGB r g b)
+                     $ toArray
+                     $ img :> PointProcess (fmap (clamp (0,1)))
