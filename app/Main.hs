3b9eb8362aacbe28f83c31b4ada2f6b60906f268
Ricardo Holmes <ricardoholmes2004@gmail.com>
2024-11-23 17:39:07 +0000

    Change thresholding example in main to binary image

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,7 @@ main = do args <- getArgs
           img <- readImageRGB fname
           let imgInv = img :> PointProcess (1-)
           let imgGray = img :> PointProcess rgbToGray
-          let imgThresh = img :> PointProcess rgbToGray :> PointProcess (\(Pixel1 x) -> x > 0.5) :> PointProcess (\x -> Pixel1 $ if x then 1 else 0)
+          let imgThresh = img :> PointProcess rgbToGray :> PointProcess (\(Pixel1 x) -> Pixel1 (x > 0.5))
           putStrLn "Starting original"
           writeImageRGB "out-original.png" img
           putStrLn "Finished original"
@@ -24,5 +24,5 @@ main = do args <- getArgs
           writeImageGray "out-gray.png" imgGray
           putStrLn "Finished gray"
           putStrLn "Starting thresholding"
-          writeImageGray "out-thresh.png" imgThresh
+          writeImageBinary "out-thresh.png" imgThresh
           putStrLn "Finished thresholding"
