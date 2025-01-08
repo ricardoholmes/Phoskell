a0e39c8134878974fd9876f1550a0ad2b0860340
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-01-08 15:11:34 +0000

    Convert colours to use Word8 rather than Double
    This should make the library better in the long run, especially for
    image analysis (i.e. histograms), but there are some issues with it
    currently.
    
    HSV and HSL will be left using Double for now for simplicity's sake.
    
    Convolution no longer works for most colour types, that will need to be
    fixed as soon as possible.
    
    Gamma correction is no longer exactly possible (as seen in `Main.hs`) so
    that will also need to be addressed soon (alongside other similar point
    processes).
    
    Generating images from functions is also a bit more complicated now so I
    will try to improve that somehow.

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+
 module Main where
 
 import System.Environment ( getArgs )
@@ -6,7 +8,6 @@ import Graphics.ImageProcessing.IO
 import Graphics.ImageProcessing.Processes
 import Graphics.ImageProcessing.Core.Pixel
 import Graphics.ImageProcessing.Core.Color
-import Graphics.ImageProcessing.Processes.Convolution
 import Graphics.ImageProcessing.Processes.Threshold
 
 main :: IO ()
@@ -14,15 +15,15 @@ main = do args <- getArgs
           let fname = head args
           img <- readImageRGB fname
           let img' = img
-                   :> PointProcess (**0.5)
+                  --  :> PointProcess (**0.5) -- i'll fix gamma correction shortly
                    :> PointProcess rgbToGray
-                   :> meanFilter 5
-                   :> threshold 0.5
+                  --  :> meanFilter 5 -- i'll fix convolution shortly too
+                   :> threshold 128
           writeImageRGB "input.png" img
           writeImageBinary "output.png" img'
           let custom = generateImage (Sz2 1000 1000) (\(y :. x) ->
                   let x' = fromIntegral (abs (x - 500)) / 500
                       y' = fromIntegral (abs (y - 500)) / 500
                   in Pixel3 x' ((x'*x' + y'*y') / 2) y'
-                )
+                ) :> PointProcess (fmap floor . (*255))
           writeImageRGB "custom.png" custom
