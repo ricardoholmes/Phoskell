f7fdd37f4431a98262135c85969257023a481fea
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-01-10 13:08:04 +0000

    Add visual histogram example to app

--- app/Main.hs
+++ app/Main.hs
@@ -15,17 +15,32 @@ main :: IO ()
 main = do args <- getArgs
           let fname = head args
           img <- readImageRGB fname
-          print $ histogramGray (img :> PointProcess rgbToGray)
           let img' = img
                   --  :> PointProcess (**0.5) -- i'll fix gamma correction shortly
                    :> PointProcess rgbToGray
                   --  :> meanFilter 5 -- i'll fix convolution shortly too
                    :> threshold 128
+          putStrLn "START"
           writeImageRGB "input.png" img
+          putStrLn "INPUT DONE"
           writeImageBinary "output.png" img'
+          putStrLn "OUTPUT DONE"
+
+          let hist = histogramGray (img :> PointProcess rgbToGray)
+          let mostCommon = maximum hist
+          print mostCommon
+          let imgHist = generateImage (Sz2 2048 4096) (\(y :. x) ->
+                  let x' = x `div` 16 -- 256 bars * 16 pixels = 4096 pixels
+                      height = ((hist !! x') * 2048) `div` mostCommon
+                      y' = 2048 - y -- invert to make bottom left the origin
+                  in if y' <= height then Pixel1 0 else Pixel1 255)
+          writeImageGray "histogram.png" imgHist
+          putStrLn "HISTOGRAM DONE"
+
           let custom = generateImage (Sz2 1000 1000) (\(y :. x) ->
                   let x' = fromIntegral (abs (x - 500)) / 500
                       y' = fromIntegral (abs (y - 500)) / 500
                   in Pixel3 x' ((x'*x' + y'*y') / 2) y'
                 ) :> PointProcess (fmap floor . (*255))
           writeImageRGB "custom.png" custom
+          putStrLn "CUSTOM DONE"
