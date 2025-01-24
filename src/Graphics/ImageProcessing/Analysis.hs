e7769e64e55b80873a6067c1f465d32c4abe7917
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-01-24 17:10:06 +0000

    Implement function to get image dimensions

--- src/Graphics/ImageProcessing/Analysis.hs
+++ src/Graphics/ImageProcessing/Analysis.hs
@@ -1,5 +1,12 @@
 module Graphics.ImageProcessing.Analysis (
     histogramGray,
+    imageSize
 ) where
 
-import Graphics.ImageProcessing.Analysis.Histogram
+import Graphics.ImageProcessing.Analysis.Histogram ( histogramGray )
+import Graphics.ImageProcessing.Core.Image ( Image, toArray )
+import Data.Massiv.Array ( Sz2, Size (size) )
+
+-- | Gets the dimensions of the image given.
+imageSize :: Image a -> Sz2
+imageSize = size . toArray
