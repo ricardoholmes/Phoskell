cccb0352a818e364f8f6cec2c4ed1bf2a539041a
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-01-30 15:12:28 +0000

    Make typeclass for generalisable histogram function

--- src/Graphics/ImageProcessing/Analysis/Histogram.hs
+++ src/Graphics/ImageProcessing/Analysis/Histogram.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# LANGUAGE InstanceSigs #-}
 
 module Graphics.ImageProcessing.Analysis.Histogram (
+    Histogrammable(..),
     histogram1,
     histogram2,
     histogram3,
@@ -11,6 +13,9 @@ import Graphics.ImageProcessing.Core
 import Data.Massiv.Array ( flatten, toList )
 import Data.Word (Word8)
 
+class Pixel p => Histogrammable p where
+    histogram :: Image (p Word8) -> [[Int]]
+
 -- | Histogram for single channel image
 histogram1 :: Image (Pixel1 Word8) -> [Int]
 histogram1 img = bins
@@ -18,6 +23,10 @@ histogram1 img = bins
         dropGray (Pixel1 x) = fromIntegral x
         imgList = toList $ flatten $ toArray (dropGray <$> img)
         bins = count (replicate 256 0) imgList
+
+instance Histogrammable Pixel1 where
+    histogram :: Image (Pixel1 Word8) -> [[Int]]
+    histogram = (:[]) . histogram1
 
 -- | Histogram for 2-channel image
 histogram2 :: Image (Pixel2 Word8) -> ([Int],[Int])
@@ -29,6 +38,11 @@ histogram2 img = (bins1, bins2)
         imgList2 = toList $ flatten $ toArray (takeCh2 <$> img)
         bins1 = count (replicate 256 0) imgList1
         bins2 = count (replicate 256 0) imgList2
+
+instance Histogrammable Pixel2 where
+    histogram :: Image (Pixel2 Word8) -> [[Int]]
+    histogram img = [c1,c2]
+        where (c1,c2) = histogram2 img
 
 -- | Histogram for 3-channel image
 histogram3 :: Image (Pixel3 Word8) -> ([Int],[Int],[Int])
@@ -43,6 +57,11 @@ histogram3 img = (bins1, bins2, bins3)
         bins1 = count (replicate 256 0) imgList1
         bins2 = count (replicate 256 0) imgList2
         bins3 = count (replicate 256 0) imgList3
+
+instance Histogrammable Pixel3 where
+    histogram :: Image (Pixel3 Word8) -> [[Int]]
+    histogram img = [c1,c2,c3]
+        where (c1,c2,c3) = histogram3 img
 
 -- | Histogram for 4-channel image
 histogram4 :: Image (Pixel4 Word8) -> ([Int],[Int],[Int],[Int])
@@ -60,6 +79,11 @@ histogram4 img = (bins1, bins2, bins3, bins4)
         bins2 = count (replicate 256 0) imgList2
         bins3 = count (replicate 256 0) imgList3
         bins4 = count (replicate 256 0) imgList4
+
+instance Histogrammable Pixel4 where
+    histogram :: Image (Pixel4 Word8) -> [[Int]]
+    histogram img = [c1,c2,c3,c4]
+        where (c1,c2,c3,c4) = histogram4 img
 
 count :: [Int] -> [Int] -> [Int]
 count xs [] = xs
