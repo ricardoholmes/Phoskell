e1df8d0bc863d51f7bae48fb5d10ee6075e16925
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-01-31 13:26:16 +0000

    Improve performance of histogram generation functions

--- src/Graphics/ImageProcessing/Analysis/Histogram.hs
+++ src/Graphics/ImageProcessing/Analysis/Histogram.hs
@@ -3,6 +3,7 @@
 
 module Graphics.ImageProcessing.Analysis.Histogram (
     Histogrammable(..),
+    arrayHistogram,
     histogram1,
     histogram2,
     histogram3,
@@ -10,84 +11,76 @@ module Graphics.ImageProcessing.Analysis.Histogram (
 ) where
 
 import Graphics.ImageProcessing.Core
-import Data.Massiv.Array ( flatten, toList )
+import qualified Data.Massiv.Array as M
+import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed.Mutable (modify)
 import Data.Word (Word8)
 
+type Histogram = [Int]
+
 class Pixel p => Histogrammable p where
-    histogram :: Image (p Word8) -> [[Int]]
+    histogram :: Image (p Word8) -> [Histogram]
+
+-- | Histogram for an image's underlying array type.
+--
+-- Adapted from [Haskell Image Processing (HIP)](https://hackage.haskell.org/package/hip).
+arrayHistogram :: ImageArray Word8 -> Histogram
+arrayHistogram img = V.toList $ V.modify countBins (V.replicate 256 0)
+    where
+        incrementBin v x = modify v (+1) $ fromIntegral x
+        countBins v = M.mapM_ (incrementBin v) img
 
 -- | Histogram for single channel image
-histogram1 :: Image (Pixel1 Word8) -> [Int]
-histogram1 img = bins
+histogram1 :: Image (Pixel1 Word8) -> Histogram
+histogram1 img = hist
     where
-        dropGray (Pixel1 x) = fromIntegral x
-        imgList = toList $ flatten $ toArray (dropGray <$> img)
-        bins = count (replicate 256 0) imgList
+        img' = toArray img
+        hist = arrayHistogram (M.map (\(Pixel1 p) -> p) img')
 
 instance Histogrammable Pixel1 where
-    histogram :: Image (Pixel1 Word8) -> [[Int]]
+    histogram :: Image (Pixel1 Word8) -> [Histogram]
     histogram = (:[]) . histogram1
 
 -- | Histogram for 2-channel image
-histogram2 :: Image (Pixel2 Word8) -> ([Int],[Int])
-histogram2 img = (bins1, bins2)
+histogram2 :: Image (Pixel2 Word8) -> (Histogram, Histogram)
+histogram2 img = (hist1, hist2)
     where
-        takeCh1 (Pixel2 x _) = fromIntegral x
-        takeCh2 (Pixel2 _ x) = fromIntegral x
-        imgList1 = toList $ flatten $ toArray (takeCh1 <$> img)
-        imgList2 = toList $ flatten $ toArray (takeCh2 <$> img)
-        bins1 = count (replicate 256 0) imgList1
-        bins2 = count (replicate 256 0) imgList2
+        img' = toArray img
+        hist1 = arrayHistogram (M.map (\(Pixel2 p _) -> p) img')
+        hist2 = arrayHistogram (M.map (\(Pixel2 _ p) -> p) img')
 
 instance Histogrammable Pixel2 where
-    histogram :: Image (Pixel2 Word8) -> [[Int]]
+    histogram :: Image (Pixel2 Word8) -> [Histogram]
     histogram img = [c1,c2]
         where (c1,c2) = histogram2 img
 
 -- | Histogram for 3-channel image
-histogram3 :: Image (Pixel3 Word8) -> ([Int],[Int],[Int])
-histogram3 img = (bins1, bins2, bins3)
+histogram3 :: Image (Pixel3 Word8) -> (Histogram, Histogram, Histogram)
+histogram3 img = (hist1, hist2, hist3)
     where
-        takeCh1 (Pixel3 x _ _) = fromIntegral x
-        takeCh2 (Pixel3 _ x _) = fromIntegral x
-        takeCh3 (Pixel3 _ _ x) = fromIntegral x
-        imgList1 = toList $ flatten $ toArray (takeCh1 <$> img)
-        imgList2 = toList $ flatten $ toArray (takeCh2 <$> img)
-        imgList3 = toList $ flatten $ toArray (takeCh3 <$> img)
-        bins1 = count (replicate 256 0) imgList1
-        bins2 = count (replicate 256 0) imgList2
-        bins3 = count (replicate 256 0) imgList3
+        img' = toArray img
+        hist1 = arrayHistogram (M.map (\(Pixel3 p _ _) -> p) img')
+        hist2 = arrayHistogram (M.map (\(Pixel3 _ p _) -> p) img')
+        hist3 = arrayHistogram (M.map (\(Pixel3 _ _ p) -> p) img')
 
 instance Histogrammable Pixel3 where
-    histogram :: Image (Pixel3 Word8) -> [[Int]]
+    histogram :: Image (Pixel3 Word8) -> [Histogram]
     histogram img = [c1,c2,c3]
         where (c1,c2,c3) = histogram3 img
 
+
 -- | Histogram for 4-channel image
-histogram4 :: Image (Pixel4 Word8) -> ([Int],[Int],[Int],[Int])
-histogram4 img = (bins1, bins2, bins3, bins4)
+histogram4 :: Image (Pixel4 Word8) -> (Histogram, Histogram, Histogram, Histogram)
+histogram4 img = (hist1, hist2, hist3, hist4)
     where
-        takeCh1 (Pixel4 x _ _ _) = fromIntegral x
-        takeCh2 (Pixel4 _ x _ _) = fromIntegral x
-        takeCh3 (Pixel4 _ _ x _) = fromIntegral x
-        takeCh4 (Pixel4 _ _ _ x) = fromIntegral x
-        imgList1 = toList $ flatten $ toArray (takeCh1 <$> img)
-        imgList2 = toList $ flatten $ toArray (takeCh2 <$> img)
-        imgList3 = toList $ flatten $ toArray (takeCh3 <$> img)
-        imgList4 = toList $ flatten $ toArray (takeCh4 <$> img)
-        bins1 = count (replicate 256 0) imgList1
-        bins2 = count (replicate 256 0) imgList2
-        bins3 = count (replicate 256 0) imgList3
-        bins4 = count (replicate 256 0) imgList4
+        img' = toArray img
+        hist1 = arrayHistogram (M.map (\(Pixel4 p _ _ _) -> p) img')
+        hist2 = arrayHistogram (M.map (\(Pixel4 _ p _ _) -> p) img')
+        hist3 = arrayHistogram (M.map (\(Pixel4 _ _ p _) -> p) img')
+        hist4 = arrayHistogram (M.map (\(Pixel4 _ _ _ p) -> p) img')
 
 instance Histogrammable Pixel4 where
-    histogram :: Image (Pixel4 Word8) -> [[Int]]
+    histogram :: Image (Pixel4 Word8) -> [Histogram]
     histogram img = [c1,c2,c3,c4]
         where (c1,c2,c3,c4) = histogram4 img
 
-count :: [Int] -> [Int] -> [Int]
-count xs [] = xs
-count xs (p:ps) = count xs' ps
-    where
-        (l,x:r) = splitAt p xs
-        xs' = l ++ (x+1:r)
