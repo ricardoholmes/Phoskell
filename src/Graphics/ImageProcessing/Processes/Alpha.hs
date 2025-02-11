8e133252cb34055f7efe344a7dd5e16f23bcf1b4
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-02-11 13:18:11 +0000

    Create basic alpha channel manipulation functions

--- src/Graphics/ImageProcessing/Processes/Alpha.hs
+++ src/Graphics/ImageProcessing/Processes/Alpha.hs
@@ -1,3 +1,22 @@
 -- | Processes involving the alpha channel.
 module Graphics.ImageProcessing.Processes.Alpha (
+    addAlphaChannel,
+    dropAlphaChannel,
+    alterAlpha,
 ) where
+
+import Graphics.ImageProcessing.Core.Image (PointProcess(PointProcess))
+import Graphics.ImageProcessing.Core
+import Data.Word
+
+-- | Add an alpha channel, setting all values to their maximum.
+addAlphaChannel :: PointProcess RGB RGBA
+addAlphaChannel = PointProcess (\(Pixel3 r g b) -> Pixel4 r g b 255)
+
+-- | Remove the alpha channel, ignoring all of its values.
+dropAlphaChannel :: PointProcess RGBA RGB
+dropAlphaChannel = PointProcess (\(Pixel4 r g b _) -> Pixel3 r g b)
+
+-- | Apply a function to all values in the alpha channel.
+alterAlpha :: (Word8 -> Word8) -> PointProcess RGBA RGBA
+alterAlpha f = PointProcess (\(Pixel4 r g b a) -> Pixel4 r g b (f a))
