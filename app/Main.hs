12a9715d1d748f96accafaa2b238b5a60532f697
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-02-11 13:55:58 +0000

    Create function for overlaying images with alpha channels

--- app/Main.hs
+++ app/Main.hs
@@ -21,6 +21,9 @@ import Graphics.ImageProcessing.Transformations.Rotation
 import Graphics.ImageProcessing.Transformations.Scaling
 import Graphics.ImageProcessing.Processes.Histogram (contrastStretch, equaliseHistogram)
 import Graphics.ImageProcessing.Synthesis
+import Graphics.ImageProcessing.Processes.Alpha (addAlphaChannel, overlayImage, alterAlpha)
+import Graphics.ImageProcessing.Analysis (imageSize)
+import Graphics.ImageProcessing (Sz(Sz2))
 
 drawImgHist :: [Int] -> Pixel3 Word8 -> Image (Pixel3 Word8)
 drawImgHist hist fg = drawBarChart' (2048,1024) 0 fg hist
@@ -39,6 +42,10 @@ main = do args <- getArgs
           putStrLn "INPUT DONE"
           writeImageBinary "output.png" img'
           putStrLn "OUTPUT DONE"
+
+          let imgA = img :> addAlphaChannel
+          let (Sz2 h w) = imageSize imgA
+          writeImageRGBA "output-overlay.png" (imgA :> overlayImage (simpleGradientH (w,h) redA greenA :> alterAlpha (const 128)))
 
           writeImageRGB "stack-vertical.png" (stackVertically 0 img img)
           writeImageRGB "stack-horizontal.png" (stackHorizontally 0 img img)
--- src/Graphics/ImageProcessing/Processes/Alpha.hs
+++ src/Graphics/ImageProcessing/Processes/Alpha.hs
@@ -3,11 +3,13 @@ module Graphics.ImageProcessing.Processes.Alpha (
     addAlphaChannel,
     dropAlphaChannel,
     alterAlpha,
+    overlayImage,
 ) where
 
-import Graphics.ImageProcessing.Core.Image (PointProcess(PointProcess))
+import Graphics.ImageProcessing.Core.Image (PointProcess(PointProcess), IPointProcess (IPointProcess))
 import Graphics.ImageProcessing.Core
 import Data.Word
+import Data.Ord (clamp)
 
 -- | Add an alpha channel, setting all values to their maximum.
 addAlphaChannel :: PointProcess RGB RGBA
@@ -20,3 +22,23 @@ dropAlphaChannel = PointProcess (\(Pixel4 r g b _) -> Pixel3 r g b)
 -- | Apply a function to all values in the alpha channel.
 alterAlpha :: (Word8 -> Word8) -> PointProcess RGBA RGBA
 alterAlpha f = PointProcess (\(Pixel4 r g b a) -> Pixel4 r g b (f a))
+
+-- | Overlay the image given onto the image that the process is being applied to.
+--
+-- https://en.wikipedia.org/wiki/Alpha_compositing
+overlayImage :: Image RGBA -> IPointProcess RGBA RGBA
+overlayImage img = IPointProcess (\idx (Pixel4 r1 g1 b1 a1) ->
+            let (Pixel4 r2 g2 b2 a2) = img ! idx
+                c1 = toDouble01 <$> Pixel3 r1 g1 b1
+                c2 = toDouble01 <$> Pixel3 r2 g2 b2
+                a1' = toDouble01 a1
+                a2' = toDouble01 a2
+                a = a2' + a1'*(1 - a2')
+                c2' = c2 `multScalar` a2'
+                c1' = c1 `multScalar` (a1'*(1-a2'))
+                Pixel3 r g b = fmap (/a) (c2' + c1')
+            in round . clamp (0,255) . (*255) <$> Pixel4 r g b a
+        )
+    where
+        toDouble01 :: Word8 -> Double
+        toDouble01 x = fromIntegral x / 255
