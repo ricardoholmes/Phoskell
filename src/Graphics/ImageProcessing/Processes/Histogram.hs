4e707d339f1606213225ccebf405a85e525e2197
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-03-02 13:46:35 +0000

    Remove mtl dependency from library
    Conflicts with doctest in test suite.

--- src/Graphics/ImageProcessing/Processes/Histogram.hs
+++ src/Graphics/ImageProcessing/Processes/Histogram.hs
@@ -11,7 +11,7 @@ import Data.Word (Word8)
 import Graphics.ImageProcessing.Analysis.Histogram (Histogrammable (histogram))
 import Graphics.ImageProcessing.Core.Pixel (Pixel)
 import qualified Data.Massiv.Array as M
-import Control.Monad.State.Lazy
+import Data.List (mapAccumL)
 
 contrastStretch :: (Pixel p, Histogrammable p) => MiscProcess (p Word8) (p Word8)
 contrastStretch = contrastStretchToRange (0,255)
@@ -25,14 +25,14 @@ contrastStretchToRange (l,u) = MiscProcess (\img ->
             oldRange = zipWith (-) highest lowest
             newRange = fromIntegral $ u - l
             l' = fromIntegral l
-            stretch' :: Word8 -> State Int Word8
-            stretch' p = do c <- get
-                            put (c+1)
-                            let p' = fromIntegral p
-                            let q = (p' - (lowest !! c)) * newRange
-                            let q' = (q `div` (oldRange !! c)) + l'
-                            return (fromIntegral q')
-        in M.map (\p -> evalState (mapM stretch' p) 0) img
+            stretch :: (Int,Word8) -> Word8
+            stretch (c,p) = let p' = fromIntegral p
+                                q = (p' - (lowest !! c)) * newRange
+                                q' = (q `div` (oldRange !! c)) + l'
+                            in fromIntegral q'
+            stretch' :: Pixel p => p Word8 -> p Word8
+            stretch' = fmap stretch . addChannelIndices
+        in M.map stretch' img
     )
 
 -- | Apply histogram equalisation algorithm to the image.
@@ -44,10 +44,10 @@ equaliseHistogram = MiscProcess (\img ->
             (total :: Double) = fromIntegral $ M.elemsCount img
             cdf c i = fromIntegral (sum $ take (fromIntegral i) (hist !! c))
             cdfMin = [head [idx | (v,idx) <- zip hx [0..], v > 0] | hx <- hist]
-            h c v = round ((cdf c v - (cdfMin !! c)) / (total - (cdfMin !! c)) * 255)
-            hPixel :: Word8 -> State Int Word8
-            hPixel v = do c <- get
-                          put (c+1)
-                          return $ h c v
-        in M.map (\p -> evalState (mapM hPixel p) 0) img
+            h (c,v) = round ((cdf c v - (cdfMin !! c)) / (total - (cdfMin !! c)) * 255)
+            hPixel = fmap h . addChannelIndices
+        in M.map hPixel img
     )
+
+addChannelIndices :: Pixel p => p Word8 -> p (Int,Word8)
+addChannelIndices = snd . mapAccumL (\n x -> (n+1,(n,x))) 0
