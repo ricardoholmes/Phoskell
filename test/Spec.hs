92712239008e77aaec2b44ca4d371a5f9d23de24
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-03-03 10:46:38 +0000

    Make pixels instances of Arbitrary for QuickCheck
    Also make example proposition that is tested when spec test-suite is
    run. This example fails due to lack of precision when dealing with
    Word8 (and integers in general) and will be replaced in the future.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,34 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 module Main where
 
-import Test.QuickCheck ()
+import Test.QuickCheck
+
+import Graphics.ImageProcessing.Core.Color
+import Graphics.ImageProcessing.Core
+import Control.Monad
+
+instance Arbitrary a => Arbitrary (Pixel1 a) where
+    arbitrary :: Arbitrary a => Gen (Pixel1 a)
+    arbitrary = Pixel1 <$> arbitrary
+
+instance Arbitrary a => Arbitrary (Pixel2 a) where
+    arbitrary :: Arbitrary a => Gen (Pixel2 a)
+    arbitrary = liftM2 Pixel2 arbitrary arbitrary
+
+instance Arbitrary a => Arbitrary (Pixel3 a) where
+    arbitrary :: Arbitrary a => Gen (Pixel3 a)
+    arbitrary = liftM3 Pixel3 arbitrary arbitrary arbitrary
+
+instance Arbitrary a => Arbitrary (Pixel4 a) where
+    arbitrary :: Arbitrary a => Gen (Pixel4 a)
+    arbitrary = liftM4 Pixel4 arbitrary arbitrary arbitrary arbitrary
+
+prop_hsv_hsl :: HSV -> Bool
+prop_hsv_hsl hsv = hsv == hslToHSV (hsvToHSL hsv)
+
+propsColorConversion :: IO ()
+propsColorConversion = do quickCheck prop_hsv_hsl
 
 main :: IO ()
-main = putStrLn "testing"
+main = do propsColorConversion
