bc7688bb1c81a8f59883b76646fc4d59aa107c8d
Ricardo Holmes <ricardoholmes2004@gmail.com>
2025-03-07 18:04:04 +0000

    Create simple benchmarks for point processes
    Uses the "Criterion" library.

---
+++ bench/Point.hs
@@ -0,0 +1,66 @@
+-- benchmarking of various point processes
+module Main where
+
+import Criterion.Main
+import System.Directory
+import System.Environment (getArgs, withArgs)
+import Graphics.ImageProcessing.IO
+import Graphics.ImageProcessing.Core
+import Graphics.ImageProcessing.Processes.Point
+
+-- | Folder name for outputting images
+outDir :: FilePath
+outDir = "bench-out/"
+
+-- | Benchmark RGBA image, writing it to @outDir@ with filename given
+benchRGBA :: FilePath -> Image RGBA -> Benchmark
+benchRGBA fname img = bench fname $ whnfIO (writeImageRGBA (outDir ++ fname) img)
+
+biasBenchmarks :: Image RGBA -> Benchmark
+biasBenchmarks img = bgroup "bias" [
+        benchRGBA "bias-add-50.png" (img :> addBias 50),
+        benchRGBA "bias-sub-50.png" (img :> subtractBias 50)
+    ]
+
+gainBenchmarks :: Image RGBA -> Benchmark
+gainBenchmarks img = bgroup "Gain" [
+        benchRGBA "gain-2.png" (img :> applyGain 2),
+        benchRGBA "gain-0_5.png" (img :> applyGain 0.5)
+    ]
+
+gammaBenchmarks :: Image RGBA -> Benchmark
+gammaBenchmarks img = bgroup "Gamma Correction" [
+        benchRGBA "gamma-2.png" (img :> gammaCorrect 2),
+        benchRGBA "gamma-0_5.png" (img :> gammaCorrect 0.5)
+    ]
+
+invBenchmarks :: Image RGBA -> Benchmark
+invBenchmarks img = bgroup "Color Inversion" [
+        benchRGBA "inv-colors.png" (img :> invertColors),
+        benchRGBA "inv-colors-not-alpha.png" (img :> invertColorsNotAlpha)
+    ]
+
+multipleBenchmarks :: Image RGBA -> Benchmark
+multipleBenchmarks img = bgroup "Multiple" [
+        benchRGBA "gain2-biasAdd20.png" (img :> gammaCorrect 2 :> addBias 20),
+        benchRGBA "gamma2-gain2-biasSub20.png" (img :> gammaCorrect 2 :> applyGain 2 :> subtractBias 20)
+    ]
+
+pointBenchmarks :: Image RGBA -> Benchmark
+pointBenchmarks img = bgroup "Point Processes" [
+        biasBenchmarks img,
+        gainBenchmarks img,
+        gammaBenchmarks img,
+        invBenchmarks img,
+        multipleBenchmarks img
+    ]
+
+main :: IO ()
+main = do
+        (fname:rest) <- getArgs
+        img <- readImageRGBA fname
+        putStrLn $ "Using image: '" ++ fname ++ "'"
+        createDirectoryIfMissing True outDir
+        withArgs rest $ defaultMain [
+                pointBenchmarks img
+            ]
